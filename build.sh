b4a6930993057225bbb4e5e77b6743fd2807a458
Jair Barbosa <jsbjair@gmail.com>
2018-10-15 19:36:51 +0000

    Defining current user as default user for installation
    To evitate trouble, install dev-env as current user, for
    when mount project, do not need to change any permission;

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-user="$1"
-docker build --build-arg user=$user --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t dev-env .
+docker build --build-arg user=$(whoami) --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t dev-env .
