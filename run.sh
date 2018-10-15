32caa6649bbee350d162d2a1c77f97e0ca20a4be
Jair Barbosa <jsbjair@gmail.com>
2018-10-15 19:54:27 +0000

    Adding realpath to run
    To evitate using absolute path.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-project_path="$1"
+project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
 docker run --rm --user $user --mount type=bind,source=${project_path},target=/home/$user/${project} -it dev-env:latest
