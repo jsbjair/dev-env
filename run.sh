80848555d357ce326b8dd71c3dabfd7e3690e847
Jair Barbosa <jsbjair@gmail.com>
2018-10-16 19:15:48 +0000

    Sharing X server to clipboard to work properly
    This allow running X(gui) programs into docker and make clipboard
    work correctly.

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
-docker run --rm --user $user --mount type=bind,source=${project_path},target=/home/$user/${project} -it dev-env:latest
+docker run --rm --user $user --mount type=bind,source=${project_path},target=/home/$user/${project} -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it dev-env:latest
 
