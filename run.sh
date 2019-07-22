81ba66801342e2e74571fc7c7968a0d44b137eac
Jair Barbosa <jbarbosa@xerpa.com.br>
2019-07-22 17:21:25 -0300

    [fix] indentation of run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
-docker run --rm --user $user --mount type=bind,source=${project_path},target=/home/$user/${project} -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it dev-env:latest
+docker run --rm --user $user \
+    --mount type=bind,source=${project_path},target=/home/$user/${project} \
+    -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -it dev-env:latest
 
