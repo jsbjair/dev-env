6679838eed6bcddc3a37bfd8c757ecd3a0b47c93
Jair Barbosa <jbarbosa@xerpa.com.br>
2019-07-22 17:28:51 -0300

    [add] Container name
    user name="foo" ./run.sh;
    
    if not defined, devenv is used;

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[[ -z $name ]] && name='devenv'
 project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
 docker run --rm --user $user \
+    --name $name \
     --mount type=bind,source=${project_path},target=/home/$user/${project} \
     -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
     -it dev-env:latest
