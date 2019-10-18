59f40b44cdd5659b0000d8324aa0d7bb41550dd9
Jair Barbosa <jbarbosa@xerpa.com.br>
2019-10-18 11:30:26 -0300

    [modify] Publishing ports
    Exposing container ports;
    Allow access to docker sock;

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@
 project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
-docker run --rm --user $user \
+docker run --user $user \
     --name $name \
+    --publish-all \
     --mount type=bind,source=${project_path},target=/home/$user/${project} \
     -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -it dev-env:latest
 
