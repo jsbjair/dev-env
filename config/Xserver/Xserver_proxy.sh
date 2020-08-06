6e76d02203eba1c16c404bd545fccd978646c8e1
Jair Barbosa <jsb@smartbank.com.br>
2020-08-06 14:06:52 +0000

    [modify] X config - colors

--- config/Xserver/Xserver_proxy.sh
+++ config/Xserver/Xserver_proxy.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 set -e
-sudo socat UNIX-LISTEN:/tmp/.X11-unix/X0,mode=0777,fork TCP:$X_SERVER_HOST:6000
+
+if [[ "$1" = "ip" ]]; then
+    echo "$X_SERVER_HOST"
+    exit 0
+fi
+
+_DISPLAY=$1
+X_SOCK=/tmp/.X11-unix/X${_DISPLAY}
+
+function clean(){
+    sudo rm -f $X_SOCK
+}
+clean
+
+trap clean SIGTERM SIGINT
+
+sudo socat UNIX-LISTEN:$X_SOCK,mode=0777,fork TCP:$X_SERVER_HOST:600${_DISPLAY},ignoreeof
 
